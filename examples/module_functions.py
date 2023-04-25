36c9bbe2f0951931846f221272f71fc21f51817a
Bruce Eckel <BruceTEckel@gmail.com>
2023-04-25 14:15:50 -0600

    Create module_functions.py
    Exercise module API

---
+++ examples/module_functions.py
@@ -0,0 +1,17 @@
+# Exercises all module functions, and shows that
+# the main Interpreter(id=0, ...) was already there
+from test.support import interpreters
+from pprint import pformat
+
+
+def test_module_functions():
+    interps = [interpreters.create() for _ in range(4)]
+    print(f"interps = {pformat(interps)}")
+    all = interpreters.list_all()
+    print(f"all = {pformat(all)}")
+    print(f"{interpreters.get_current() = }")
+    print(f"{interpreters.get_main() = }")
+
+
+if __name__ == "__main__":
+    test_module_functions()
