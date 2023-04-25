a95768eee5166a88f05684c54d6eb07b04716211
Bruce Eckel <BruceTEckel@gmail.com>
2023-04-25 11:05:47 -0600

    Update exception_handling.py
    Works correctly if instructions in NOTES.md are followed.

--- examples/exception_handling.py
+++ examples/exception_handling.py
@@ -1,17 +1,14 @@
 import textwrap as tw
 from test.support import interpreters
 
-ISSUE = "Thought this was fixed; see NOTES.md"
-
 def test_exception_handling():
-    print(ISSUE); return
     interp = interpreters.create()
     try:
         interp.run(tw.dedent("""
             raise KeyError
             """))
     except interpreters.RunFailedError as exc:
-        print(f"got the error from the subinterpreter: {exc}")
+        print(f"Expecting KeyError from the subinterpreter, got: {exc}")
 
 
 if __name__ == "__main__":
