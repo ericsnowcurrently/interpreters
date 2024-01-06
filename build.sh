f2827f905924b5207a33bce3cdb4d812d4bd2cb4
Eric Snow <ericsnowcurrently@gmail.com>
2024-01-05 20:07:19 -0700

    Check the built modules after building.

--- build.sh
+++ build.sh
@@ -31,7 +31,7 @@ fi
 
 
 echo "building extension modules with $("$PYTHON_312" -V)"
-venv_root=build/venv_312
+venv_root=$PROJECT_DIR/build/venv_312
 #"$PYTHON_312" -m venv --clear $venv_root
 venv_exe=$venv_root/bin/python3.12
 
@@ -42,10 +42,12 @@ venv_exe=$venv_root/bin/python3.12
 "$venv_exe" -m pip install --upgrade build
 "$venv_exe" -P -m build --no-isolation
 )
+
 #interpreters_3_12-0.0.1.1.tar.gz
 #interpreters_3_12-0.0.1.1-cp312-cp312-linux_x86_64.whl
+DIST_TARBALL=$(ls dist/interpreters_3_12-*.tar.gz)
+DIST_WHEEL=$(ls dist/interpreters_3_12-*.whl)
 
-# XXX This must be done using Python 3.12.
 #mkdir $UNPACKED_DIR
 #tar -C $UNPACKED_DIR -xzf "$(ls dist/*.tar.gz)"
 #&>/dev/null pushd $UNPACKED_DIR
@@ -56,6 +58,14 @@ venv_exe=$venv_root/bin/python3.12
 #)
 #&>/dev/null popd
 #rm -r $UNPACKED_DIR
+
+(set -x
+"$venv_exe" -m pip install $DIST_TARBALL
+"$venv_exe" -c 'import _interpreters'
+"$venv_exe" -c 'import _interpchannels'
+"$venv_exe" -c 'import _interpqueues'
+"$venv_exe" -m pip uninstall interpreters_3_12
+)
 
 &>/dev/null popd
 
