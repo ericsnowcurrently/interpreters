64b9b0b737042fe438f200dd357f8eb65929c1ab
Eric Snow <ericsnowcurrently@gmail.com>
2024-01-09 18:43:04 -0700

    Clean up.

--- scripts/_cpython.py
+++ scripts/_cpython.py
@@ -1,7 +1,6 @@
 #import re
 
 import _git
-import _cparser
 
 
 def _assert_valid_path(path, files):
@@ -30,7 +29,6 @@ def resolve_repo(repo=None):
 def get_downloader(repo, revision, downdir, *,
                    files=None,
                    knowndirs=None,
-                   fallback=False,
                    ):
     repo = resolve_repo(repo)
     if knowndirs is None:
@@ -40,8 +38,7 @@ def get_downloader(repo, revision, downdir, *,
         _assert_valid_path(path, files)
         target = f'{downdir}/...'
         target, _, _, _, _ = repo.download(path, target, revision,
-                                           makedirs=knowndirs,
-                                           fallback=fallback)
+                                           makedirs=knowndirs)
         if files is not None:
             files[path] = target
         return target
@@ -52,7 +49,6 @@ def get_downloader(repo, revision, downdir, *,
 def get_alts_downloader(repo, revision, downdir, altpaths, *,
                         files=None,
                         knowndirs=None,
-                        fallback=False,
                         ):
     repo = resolve_repo(repo)
     if altpaths is not None:
@@ -71,110 +67,9 @@ def get_alts_downloader(repo, revision, downdir, altpaths, *,
             altpath = None
         target, _, downpath, _, _ = repo.download(path, target, revision,
                                                   altpath=altpath,
-                                                  makedirs=knowndirs,
-                                                  fallback=fallback)
+                                                  makedirs=knowndirs)
         if files is not None:
             files[path] = target
         return target, downpath, altpath
 
     return download, altpaths
-
-
-#############################
-# includes
-
-def _resolve_include(include):
-    if include.startswith('pycore_'):
-        include = f'internal/{include}'
-    #elif '/' not in include:
-    return f'/Include/{include}'
-
-
-#def iter_includes(filename, seen=None):
-#    if seen is None:
-#        seen = {}
-#
-#    path = _resolve_include('Python.h')
-#    if path not in seen:
-#        seen[path] = None
-#        yield path
-#
-#    with open(filename) as infile:
-#        for name, kind in _cparser.iter_used(infile, filename):
-#            if kind == 'include':
-#                path = _resolve_include(name)
-#                if path in seen:
-#                    continue
-#                seen[path] = None
-#                yield path
-#            elif kind in _cparser.KINDS:
-#                continue
-#            else:
-#                raise NotImplementedError((name, kind))
-
-
-def iter_includes(filename, seen=None):
-    if seen is None:
-        seen = {}
-
-    path = _resolve_include('Python.h')
-    if path not in seen:
-        seen[path] = None
-        yield path
-
-    with open(filename) as infile:
-        for directive in _cparser.PreprocessorDirective.iter_from_lines(infile):
-            if directive.kind != 'include':
-                continue
-            subkind, name = directive.value
-            if subkind == 'system':
-                continue
-            path = _resolve_include(name)
-            if path in seen:
-                continue
-            seen[path] = None
-            yield path
-
-
-def list_public_headers(repo=None, revision=None, *, fallback=False):
-    repo = resolve_repo(repo)
-    text = repo.read('/Include/Python.h', revision,
-                     encoding='utf-8',
-                     fallback=fallback)
-    lines = text.splitlines()
-
-    includes = []
-    for directive in _cparser.PreprocessorDirective.iter_from_lines(lines):
-        if directive.kind != 'include':
-            continue
-        subkind, name = directive.value
-        if subkind == 'system':
-            continue
-        path = _resolve_include(name)
-        if path not in includes:
-            includes.append(path)
-#    for name, kind in _cparser.iter_used(text.splitlines()):
-#        if kind != 'include':
-#            continue
-#        path = _resolve_include(name)
-#        if path not in includes:
-#            includes.append(path)
-    assert includes
-    return includes
-
-
-#############################
-# dependencies
-
-def analyze_dependencies(lines, filename=None, *,
-                         download_include=None,
-                         recurse=False,
-                         ):
-    # Python.h is always implied.
-    dep = _cparser.CDependencyInclude.from_location(
-                        'include-user', 'Python.h', filename)
-    implied = [dep]
-
-    yield from _cparser.analyze_dependencies(infile, filename, implied,
-                                             download_include=download_include,
-                                             recurse=recurse)
