b0e9857b98b8f7fd771d489c18bb4b33b77d2bdf
Eric Snow <ericsnowcurrently@gmail.com>
2024-01-11 17:50:46 -0700

    Fix the build script.

--- cpython_helpers.sh
+++ cpython_helpers.sh
@@ -1,114 +1,330 @@
 
-CPYTHON_UPSTREAM=https://github.com/python/cpython
-CPYTHON_SRC=cpython
-CPYTHON_BUILD=cpython_312_build
-CPYTHON_INSTALL=cpython_312_install
+function log() {
+    1>&2 echo $@
+}
 
-function find-cpython() {
+function run() {
+    log "+ $@"
+    1>&2 "$@"
+}
+
+function utcnow() {
+    date --utc +'%Y%m%d-%H%M%S'
+}
+
+
+#######################################
+# $PATH
+
+function check-python-on-path() {
+    local executable=$1
+    local version=$2
+    local found=
+
+    log "looking for ${executable} on \$PATH..."
+    found=$(which "$executable")
+    if [ -z "$found" ]; then
+        log "...not found"
+        return 1
+    else
+        log "...checking version..."
+        local verstr=$(2>&1 "$found" --version)
+        if echo $verstr | grep -P "^Python ${version}(\.\d+)?$"; then
+            log "...okay"
+        else
+            log "...wrong version ($verstr)"
+            return 1
+        fi
+    fi
+    echo "$found"
+    return 0
+}
+
+function find-cpython-on-path() {
     local version=$1
-    local outfile=$2
     local found=
 
     if [ -e "$version" ]; then
-        1>&2 echo 'find-cpython: missing version arg'
-        return
+        log 'find-cpython: missing version arg'
+        return 1
     fi
 
-    echo "looking for python${version} on \$PATH..."
-    # XXX Find it.
-
-    if [ -n "$found" ]; then
-        echo "found: $found"
-    else
-        echo "python${version} not found"
+    found=$(check-python-on-path python${version} $version)
+    if [ -z "$found" ]; then
+        found=$(check-python-on-path python3 $version)
+        if [ -z "$found" ]; then
+            found=$(check-python-on-path python $version)
+            if [ -z "$found" ]; then
+                log "Python $version not found"
+                return 1
+            fi
+        fi
     fi
-    if [ -n "$outfile" ]; then
-        echo -n $found > "$outfile"
-    else
-        # The caller can capture the last line.
-        echo $found
-    fi
+    echo $found
+    return 0
 }
+
+
+#######################################
+# local repo
+
+CPYTHON_UPSTREAM=https://github.com/python/cpython
 
 function ensure-cpython-source() {
     local version=$1
     local srcdir=$2
     if [ -e $srcdir ]; then
-        echo "found local clone: $srcdir"
-        echo "updating..."
-        (set -x
-        git -c $srcdir checkout $version
-        git -c $srcdir pull
-        )
+        log "found local clone: $srcdir"
+        log "updating..."
+        if ! run git -C "$srcdir" checkout $version; then
+            return 1
+        elif ! run git -C "$srcdir" pull; then
+            return 1
+        fi
     else
-        echo "cloning CPython locally..."
-        (set -x
-        git clone --branch $version $CPYTHON_UPSTREAM $srcdir
-        git -c $srcdir checkout $version
-        )
+        log "cloning CPython locally..."
+        if ! run git clone --branch $version $CPYTHON_UPSTREAM "$srcdir"; then
+            return 1
+        elif ! run git -C "$srcdir" checkout $version; then
+            return 1
+        fi
     fi
+    return 0
 }
+
+
+#######################################
+# custom build/install
 
 function build-cpython() {
-    local srcdir=$(realpath $1)
+    local debug=$BUILD_DEBUG
+    if [ "$1" = '--debug' ]; then
+        debug=1
+        shift
+    elif [ "$1" = '--no-debug' ]; then
+        debug=0
+        shift
+    elif [ -z "$debug" ]; then
+        debug=0
+    fi
+    local srcdir=$1
     local builddir=$2
     local installdir=$3
+    if [ -z "$installdir" ]; then
+        log "missing installdir (arg #3)"
+        return 1
+    fi
+    local exitcode=0
 
-    mkdir -p "$builddir"
-    &>/dev/null pushd $builddir
-    if [ -z "$BUILD_DEBUG" ]; then
-        echo "building..."
-        (set -x
-        $srcdir/configure \
-            --with-system-ffi \
-            --prefix=$installdir
-        )
+    local config_args=(
+        "--prefix=$installdir"
+    )
+    if [ "$debug" -eq 0 ]; then
+        log "building in $builddir..."
     else
-        echo "building (debug)..."
-        (set -x
-        $srcdir/configure \
-            --with-system-ffi \
-            --with-pydebug \
-            CFLAGS=-O0 \
-            --prefix=$installdir
+        log "building (debug) in $builddir..."
+        config_cmd+=(
+            "--with-pydebug"
+            "CFLAGS=-O0"
         )
     fi
-    (set -x
-    make -j8
-    )
+
+    1>&2 mkdir -p "$builddir"
+    &>/dev/null pushd "$builddir"
+    if ! run $srcdir/configure ${config_args[@]}; then
+        exitcode=1
+    elif ! run make -j8; then
+        exitcode=1
+    fi
     &>/dev/null popd
+    return $exitcode
 }
 
-function ensure-cpython-installed() {
+function version-from-built-cpython() {
+    local builddir=$1
+    grep '^VERSION=' "$builddir/python-config" | awk -F'"' '{print $2}'
+}
+
+function installdir-from-built=cpython() {
+    local builddir=$1
+    grep '^prefix=' "$builddir/python-config" | awk -F'"' '{print $2}'
+}
+
+function resolve-installed-cpython() {
+    local installdir=$1
+    local version=$2
+    echo "$installdir/bin/python${version}"
+}
+
+function install-built-cpython() {
+    local builddir=$1
+    local installdir=$2
+    local version=$3
+    if [ -z "$installdir" ]; then
+        installdir=$(installdir-from-built-cpython "$builddir")
+        if [ -z "$installdir" ]; then
+            log "could not determine installdir (did you already build?)"
+            return 1
+        fi
+    fi
+    if [ -z "$version" ]; then
+        version=$(version-from-built-cpython "$builddir")
+        if [ -z "$version" ]; then
+            log "could not determine version (did you already build?)"
+            return 1
+        fi
+    fi
+    local executable=$(resolve-installed-cpython "$installdir" $version)
+
+    log "installing..."
+    &>/dev/null pushd "$builddir"
+    if ! run make install; then
+        &>/dev/null popd
+        return 1
+    elif [ ! -e "$executable" ]; then
+        log "something went wrong ($exectuable not found)"
+        &>/dev/null popd
+        return 1
+    fi
+    &>/dev/null popd
+    echo "$executable"
+    return 0
+}
+
+
+#######################################
+# local build project
+
+function resolve-projroot() {
+    local workdir=$1
+    if [ -z "$workdir" ]; then
+        workdir="."
+    fi
+    echo "$1/cpython"
+}
+
+function resolve-local-srcdir() {
+    # We expect .../cpython
+    local projdir=$1
+    echo "$projdir/source"
+}
+
+function resolve-local-builddir() {
+    local version=$1
+    # We expect .../cpython
+    local projdir=$2
+    echo "$projdir/build_${version}"
+}
+
+function resolve-local-installdir() {
+    local version=$1
+    # We expect .../cpython
+    local projdir=$2
+    echo "$projdir/installed_${version}"
+}
+
+function version-from-local-dir() {
+    local name=$(basename "$1")
+    echo $name | grep -P -o '(?<=^build_|^install_)\d+\.\d+'
+}
+
+function prep-local-project() {
+    local version=$1
+    # We expect .../cpython
+    local projdir=$2
+    local srcdir=$(resolve-local-srcdir "$projdir")
+    if [ -z "$srcdir" ]; then
+        return 1
+    fi
+
+    1>&2 mkdir -p "$projdir"
+    ensure-cpython-source $version "$srcdir"
+}
+
+function build-local-cpython() {
+    local version=$1
+    # We expect .../cpython
+    local projdir=$2
+    local srcdir=$(resolve-local-srcdir "$projdir")
+    local builddir=$(resolve-local-builddir $version "$projdir")
+    local installdir=$(resolve-local-installdir $version "$projdir")
+    if [ -z "$srcdir" -o -z "$builddir" -o -z "$installdir" ]; then
+        return 1
+    fi
+
+    build-cpython "$srcdir" "$builddir" "$installdir"
+}
+
+function install-local-cpython() {
+    local version=$1
+    # We expect .../cpython
+    local projdir=$2
+    local builddir=$(resolve-local-builddir $version "$projdir")
+    local installdir=$(resolve-local-installdir $version "$projdir")
+    if [ -z "$builddir" -o -z "$installdir" ]; then
+        return 1
+    fi
+
+    install-built-cpython "$builddir" "$installdir" $version
+}
+
+function find-local-cpython() {
+    local version=$1
+    local workdir=$2
+    local projdir=$(resolve-projroot "$workdir")
+    local installdir=$(resolve-local-installdir $version "$projdir")
+    local executable=$(resolve-installed-cpython "$installdir" $version)
+    if [ -z "$projdir" -o -z "$installdir" -o -z "$executable" ]; then
+        return 1
+    fi
+
+    if [ ! -e $executable ]; then
+        return 1
+    fi
+    echo $executable
+    return 0
+}
+
+function build-and-install-local-cpython() {
+    local version=$1
+    local workdir=$2
+    local projdir=$(resolve-projroot "$workdir")
+    if [ -z "$projdir" ]; then
+        return 1
+    fi
+
+    if ! prep-local-project $version "$projdir"; then
+        return 1;
+    elif ! build-local-cpython $version "$projdir"; then
+        return 1
+    fi
+    install-local-cpython $version "$projdir"
+}
+
+
+#######################################
+
+function ensure-cpython() {
     local version=$1
     local workdir=$(realpath $2)
-    local outfile=$3
-    local installdir=$workdir/cpython_${version}_install
-    local executable="$installdir/bin/python${version}"
+    local found=
 
-    mkdir -p $workdir
-    if [ -e $executable ]; then
-        echo "found locally built: $executable"
-    else
-        local srcdir=$workdir/cpython
-        local builddir=$workdir/cpython_${version}
-
-        ensure-cpython-source $version "$srcdir"
-
-        build-cpython "$srcdir" "$builddir" "$installdir"
-
-        echo "installing..."
-        &>/dev/null pushd $builddir
-        (set -x
-        make install
-        )
-        &>/dev/null popd
+    found=$(find-cpython-on-path $version)
+    if [ -z "$found" ]; then
+        log "falling back to a locally built python${version}..."
+        found=$(find-local-cpython $version "$workdir")
+        if [ -n "$found" ]; then
+            log "found locally built: $found"
+        else
+            found=$(build-and-install-local-cpython $version $workdir)
+            if [ -z "$found" ]; then
+                return 1
+            fi
+        fi
     fi
-    if [ -n "$outfile" ]; then
-        mkdir -p $(dirname $outfile)
-        echo -n $executable > "$outfile"
-    else
-        # The caller can capture the last line.
-        echo $executable
+    if [ -z "$found" ]; then
+        return 1
     fi
+    echo $found
+    return 0
 }
